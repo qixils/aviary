abe98a00b6a7164a1cf2cb5dcfadba09bb6b8026
Lexi <lexi@qixils.dev>
2022-07-12 10:05:24 -0400

    remove kotlin dep in favor of java-library

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,16 @@
 plugins {
     application
     id("java")
+    id("java-library")
     id("com.github.johnrengelman.shadow") version ("7.1.1") apply false
-    kotlin("jvm") version "1.7.10" apply false // TODO: depending on kotlin just for api() is lame
 }
 
 subprojects {
     apply {
         plugin("org.gradle.application")
         plugin("java")
+        plugin("java-library")
         plugin("com.github.johnrengelman.shadow")
-        plugin("org.jetbrains.kotlin.jvm")
     }
 
     repositories {
